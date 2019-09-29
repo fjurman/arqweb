d150c39fc6aa189e79ae142edeabe148cdfd585b
fjurman <fjurman@hotmail.com>
2019-09-29 12:44:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,25 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const moment     = require('moment');
-const clientsService = require('./services/clientsService');
-
+const alumnosService = require('./services/alumnosService');
 const app = express();
 
-
-
 // parse body as json
-
 app.use(bodyParser.json());
 
-
 // all requests
-
 app.use((req, res, next) => {
-
     console.log(`${req.method}: ${req.path} - ${moment().format(moment.HTML5_FMT.DATETIME_LOCAL_MS)}`);
-
     next();
-
 });
 
-
-
 // get home
-
 app.get('/', async (req, res) => {
-
-    let clients = await clientsService.getAll();
-
+    let alumnos = await alumnosService.getAll();
     let list = [];
-
-    clients.forEach((c) => {
-
-        list.push(`<li>${c.nombre} con id: ${c.id}</li>`);
-
+    alumnos.forEach((a) => {
+        list.push(`<li>${a.nombre} con id: ${a.id}</li>`);
     });
-
     res.send(
         `<html>
             <head>
@@ -52,65 +34,34 @@ app.get('/', async (req, res) => {
     );
 });
 
-
-
 // api
-
-
-app.get('/api/clients', async (req, res) => {
-
-    let clients = await clientsService.getAll();
-
-    res.json(clients);
-
+app.get('/v1/api/alumnos', async (req, res) => {
+    let alumnos = await alumnosService.getAll();
+    res.json(alumnos);
 });
 
-
-
-// get clients by id
-
-app.get('/api/clients/:id', async (req, res) => {
-
+// get alumnos by id
+app.get('/v1/api/alumnos/:id', async (req, res) => {
     try {
-
-        let cli = await clientsService.getById(req.params.id);
-
-        res.json(cli);
-
+        let alumno = await alumnosService.getById(req.params.id);
+        res.json(alumno);
     } catch(ex) {
-
         res.status(404).end();
-
     }
-
 });
-
 
 // add new client - Content Type: json
-
-app.post('/api/clients', (req, res) => {
-
-    clientsService.add(req.body);
-
+app.post('/v1/api/alumnos', (req, res) => {
+    alumnosService.add(req.body);
     res.status(201).send(req.body);
-
 });
 
-
-
-app.delete('/api/clients/:id', (req, res) => {
-
-    clientsService.deleteById(req.params.id);
-
+app.delete('/v1/api/alumnos/:id', (req, res) => {
+    alumnosService.deleteById(req.params.id);
     res.status(204).end();
 });
 
-
-
 // start server
-
 app.listen(process.env.PORT || 3000, function () {
-
     console.log('API andando con express...');
-
 });
