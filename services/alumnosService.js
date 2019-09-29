f86f3346b641344e889e26ea8d3aa8ba2d911801
fjurman <fjurman@hotmail.com>
2019-09-29 12:52:13 -0300

    Update alumnosService.js

--- services/alumnosService.js
+++ services/alumnosService.js
@@ -4,9 +4,24 @@ function AlumnosService() {
     console.log('Init user service');
 
     this._alumnos = [
-        {id: 1, nombre: 'Diego', edad:20 },
-        {id: 2, nombre: 'Agustin', edad: 22 },
-        {id: 3, nombre: 'Alvaro', edad:24 }
+        {id: 1, nombre: 'Nombre 1', edad:20, fechaingreso: '01-03-2016',
+        materias:[
+            {id:1,nombre:"Arq. Web"},
+            {id:31,nombre:"Analisis Matemático"}
+            ],
+        },
+        {id: 2, nombre: 'Nombre 2', edad:22, fechaingreso: '01-03-2019',
+        materias:[
+            {id:12,nombre:"Fisica 1"},
+            {id:11,nombre:"Algebra"}
+            ],
+        },
+        {id: 3, nombre: 'Nombre 3', edad:23, fechaingreso: '01-08-2017',
+        materias:[
+            {id:172,nombre:"Historia"},
+            {id:97,nombre:"Economia"}
+            ],
+        },
     ];
 };
 
